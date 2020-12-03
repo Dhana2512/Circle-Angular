ca960bbc715f8300ad4b5ef485ddcb9108e343c6
dhana <dhanavidhu256@gmail.com>
2020-12-03 19:57:16 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'circle';
+  users = [
+    {
+      name: 'Dhana',
+      color: 'red'
+    },
+    {
+      name: 'Prakash',
+      color: 'powderblue'
+    },
+    {
+      name: 'Yuvaraj',
+      color: 'red'
+    },
+    {
+      name: 'Shyamala',
+      color: 'powderblue'
+    },
+    {
+      name: 'Nisha',
+      color: 'red'
+    },
+    {
+      name: 'Vandhana',
+      color: 'powderblue'
+    },
+    {
+      name: 'Manigandan',
+      color: 'red'
+    },
+    {
+      name: 'Chenchaiah',
+      color: 'powderblue'
+    },
+    {
+      name: 'Devendran',
+      color: 'red'
+    },
+    {
+      name: 'Ramkumar',
+      color: 'powderblue'
+    },
+    {
+      name: 'Paavanakumar',
+      color: 'red'
+    },
+    {
+      name: 'Nagaraja',
+      color: 'powderblue'
+    },
+    {
+      name: 'Vimal',
+      color: 'red'
+    },
+    {
+      name: 'Dhamodaran',
+      color: 'powderblue'
+    },
+    {
+      name: 'Dharankumar',
+      color: 'red'
+    },
+    {
+      name: 'Govardanan',
+      color: 'powderblue'
+    },
+    {
+      name: 'Harikrishna',
+      color: 'red'
+    },
+    {
+      name: 'Kaushik',
+      color: 'powderblue'
+    },
+    {
+      name: 'Nethaji',
+      color: 'red'
+    },
+    {
+      name: 'Purushothaman',
+      color: 'powderblue'
+    },
+    {
+      name: 'Rajesh',
+      color: 'powderblue'
+    },
+    {
+      name: 'Udhayakumar',
+      color: 'powderblue'
+    },
+  ]
 }
